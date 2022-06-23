a5c3609226a57a306a253ba1834e1811ece07fb1
peanutfisher <meifajia@outlook.com>
2022-06-24 00:11:54 +0800

    used AppRunner API to rewrite app.py(Django)

--- app.py
+++ app.py
@@ -9,14 +9,16 @@ from aiohttp import web
 async def index(request):
     return web.Response(body=b'<h1>AWESON</h1>', content_type='text/html')
 
-async def init(loop):
-    app = web.Application(loop = loop)
+async def init():
+    app = web.Application()
     app.router.add_route('GET', '/', index)
-    srv = await loop.create_server(app.make_handler(), '127.0.0.1', '9000')
+    app_runner = web.AppRunner(app)
+    await app_runner.setup()
+    srv = await loop.create_server(app_runner.server, '127.0.0.1', '9000')
     logging.info('server started at http://127.0.0.1:9000...')
     return srv
 
 loop = asyncio.get_event_loop()
-loop.run_until_complete(init(loop))
+loop.run_until_complete(init())
 loop.run_forever()
 
